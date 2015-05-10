e98e3c3d215b784dbf217199b74852ddb895e86a
jindai1811 <jindai1811@gmail.com>
2015-05-10 16:39:34 +0800

    2015年5月10日 16:35:36 ，完成了二维码生成

--- src/main/java/com/serket/cloud/server/PrescriptionServerImpl.java
+++ src/main/java/com/serket/cloud/server/PrescriptionServerImpl.java
@@ -1,5 +1,7 @@
 package com.serket.cloud.server;
 
+import com.serket.cloud.service.PrescriptionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +13,19 @@ import java.util.Map;
  * Created by Administrator on 2015/5/10.
  */
 @RestController
-public class PrescriptionServerImpl implements  PrescriptionServer {
+public class PrescriptionServerImpl implements PrescriptionServer {
+    @Autowired
+    private PrescriptionService service;
 
-    public Map<String, String> businessLogin(@PathVariable("appid") String appid, @PathVariable("content") String content, @RequestHeader("accessToken") String accessToken) {
-        Map<String,String>  map=new HashMap<String, String>();
+    public Map<String, String> businessLogin(@PathVariable("appid") String appid, @PathVariable("content") String content) {
+        String QRCode = service.generateQRCode(appid, content);
+        Map<String, String> map = new HashMap<String, String>();
         // ×´Ì¬
-        map.put("state","ok");
+        map.put("state", "ok");
         // ¶þÎ¬ÂëÄÚÈÝ
-        map.put("QRCode","sFopJ9lMmLl4u-ad61ojKpS0TolhN2s3SnHoI2Mh5GgdiYb35i-7DG2T2CDyQKMe");
+        map.put("QRCode", QRCode);
         return map;
     }
+
+
 }
